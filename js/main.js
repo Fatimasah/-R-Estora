547ab5a4f87d356663ed4f8650fcdf8341273825
Fatima Sahmoud <fatimasahmouod@hotmail.com>
2019-10-11 14:52:15 +0300

    edit matches tab and single page and live

---
+++ js/main.js
@@ -0,0 +1,36 @@
+// 
+
+$(document).ready(function () {
+
+    $("#owl-demo").owlCarousel({
+        rtl: true,
+        loop: false,
+        margin: 10,
+        nav: false,
+        responsiveClass: true,
+        responsive: {
+            0: {
+                items: 2
+            },
+            600: {
+                items: 3
+            },
+            800: {
+                items: 4
+            },
+            1199: {
+                items: 7
+            }
+        }
+    });
+
+
+
+
+
+
+
+});
+
+
+// plyr.setup("#plyr-video");
