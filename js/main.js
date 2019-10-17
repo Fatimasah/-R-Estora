8507045b98a33563de18eab5b14f2d35cf23084f
Fatima Sahmoud <fatimasahmouod@hotmail.com>
2019-10-17 07:04:55 +0300

    add other pages and make some edit

--- js/main.js
+++ js/main.js
@@ -26,26 +26,65 @@ $(document).ready(function () {
     // tabs carousl
 
     $("#owl-demo2").owlCarousel({
-      rtl: true,
-      loop: false,
-      margin: 10,
-      nav: false,
-      responsiveClass: true,
-      responsive: {
-          0: {
-              items: 2
-          },
-          600: {
-              items: 3
-          },
-          800: {
-              items: 4
-          },
-          1199: {
-              items: 7
-          }
-      }
-  });
+        rtl: true,
+        loop: false,
+        margin: 10,
+        nav: false,
+        responsiveClass: true,
+        responsive: {
+            0: {
+                items: 2
+            },
+            600: {
+                items: 3
+            },
+            800: {
+                items: 4
+            },
+            1199: {
+                items: 7
+            }
+        }
+    });
+
+    $('.nav-link').click(function () {
+        $('.nav-link').removeClass('active');
+        $(this).addClass('active');
+        ($(this).attr("aria-selected") === "true"); 
+        $('.nav-link').removeClass('active');
+
+
+
+    });
+    $('.owl-item').click(function () {
+        $('.owl-item').removeClass('active');
+        $(this).addClass('active');
+
+    });
+
+    $("#owl-demo3").owlCarousel({
+        rtl: true,
+        loop: false,
+        margin: 10,
+        nav: true,
+        // dots: false,
+        responsiveClass: true,
+        responsive: {
+            0: {
+                items: 1
+            },
+           400: {
+                items: 2
+            },
+            800: {
+                items: 3
+            },
+            1199: {
+                items: 4
+            }
+        }
+    });
+
 
 
 
@@ -55,17 +94,17 @@ $(document).ready(function () {
 
 
 // pagination
-window.onload = function(){
+window.onload = function () {
     var paginationPage = parseInt($('.cdp').attr('actpage'), 10);
-    $('.cdp_i').on('click', function(){
-      var go = $(this).attr('href').replace('#!', '');
-      if (go === '+1') {
-        paginationPage++;
-      } else if (go === '-1') {
-        paginationPage--;
-      }else{
-        paginationPage = parseInt(go, 10);
-      }
-      $('.cdp').attr('actpage', paginationPage);
+    $('.cdp_i').on('click', function () {
+        var go = $(this).attr('href').replace('#!', '');
+        if (go === '+1') {
+            paginationPage++;
+        } else if (go === '-1') {
+            paginationPage--;
+        } else {
+            paginationPage = parseInt(go, 10);
+        }
+        $('.cdp').attr('actpage', paginationPage);
     });
-  };
+};
